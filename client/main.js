a044b78d90a95ae806aef74a72dfbbb8e284291a
leogonzalez <leo.lujan.gonzalez@gmail.com>
2017-05-11 10:47:33 -0300

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {Players,calculatePlayersPositions} from './../imports/api/players';
+import App from './../imports/ui/App';
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find({},{sort:{ score:-1}}).fetch();
+    let positionedPlayer = calculatePlayersPositions(players);
+    let title = 'Score Keep';
+    ReactDOM.render(<App title={title} players={positionedPlayer}/>, document.getElementById('app'));
+  });
+});
---
+++ server/main.js
@@ -0,0 +1,42 @@
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+
+Meteor.startup(() => {
+
+
+
+});
+
+
+
+// //Object Spread Operator
+//
+// let user = {
+//   name: 'Andrew',
+//   location: 'Pil'
+// };
+//
+// let person = {
+//   ...user,
+//   age: 25
+// };
+//
+// console.log(person);
+//
+// // Objet Prop shorthand
+//
+// let house = {
+//   bedrooms:2,
+//   bathrooms:1.5
+// };
+//
+// let yearBuilt =1995;
+//
+// let newo = {
+//   ...house,
+//   bedrooms:3,
+//   yearBuilt,
+//   flooring: 'Carpet'
+// }
+//
+// console.log(newo);
